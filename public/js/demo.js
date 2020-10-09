a7d29568d4768e500835a0fb76fccb1934536991
Rahul Sonone <rahulsonone1234@gmail.com>
2020-10-09 16:31:53 +0530

    first commit

---
+++ public/js/demo.js
@@ -0,0 +1,26 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+// This can be used to set the Particles Effects. Check README for more details!
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+}, true);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
